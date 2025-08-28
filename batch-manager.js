d924dedd1493d03b7dbe39a1c1a6b04880c7dfa5
Sidharth T <sidhartht900@gmail.com>
2025-08-28 13:42:05 +0530

    list updated

--- batch-manager.js
+++ batch-manager.js
@@ -56,7 +56,7 @@ let defaultBatches = {
         "Muhammed Shamshad",
         "Najila (RP)",
         "Nazneen (RP)",
-        "Sahla (RP)",
+        "Sahla ",
         "Shahitha (RP)",
         "Shahna (RP)",
       ],
