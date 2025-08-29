695c9d2b4fc9edb29371a3559b73b0f51999922e
SIDHARTH T <sidhartht900@gmail.com>
2025-08-29 12:03:31 +0530

    Config page updated

--- firebase.js
+++ firebase.js
@@ -4,17 +4,17 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 // 🔑 Your Firebase config (already valid for your project)
-// import { firebaseConfig } from "./config.js";
+import { firebaseConfig } from "./config.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAFe3jkeenflwT1boIfiYYJzseImeoB8gg",
-  authDomain: "report-e8db1.firebaseapp.com",
-  projectId: "report-e8db1",
-  storageBucket: "report-e8db1.firebasestorage.app",
-  messagingSenderId: "484990595847",
-  appId: "1:484990595847:web:f0c319c020f1636ea14ba2",
-  measurementId: "G-SHW5K7DDK4"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAFe3jkeenflwT1boIfiYYJzseImeoB8gg",
+//   authDomain: "report-e8db1.firebaseapp.com",
+//   projectId: "report-e8db1",
+//   storageBucket: "report-e8db1.firebasestorage.app",
+//   messagingSenderId: "484990595847",
+//   appId: "1:484990595847:web:f0c319c020f1636ea14ba2",
+//   measurementId: "G-SHW5K7DDK4"
+// };
 
 // ✅ Initialize Firebase & Firestore
 const app = initializeApp(firebaseConfig);
