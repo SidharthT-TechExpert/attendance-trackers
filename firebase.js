9bff2e3708a622db7dcbf3cdd2a48fc151bc10b5
SIDHARTH T <sidhartht900@gmail.com>
2025-08-29 12:41:54 +0530

    FIrebase updated

--- firebase.js
+++ firebase.js
@@ -4,17 +4,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 // 🔑 Your Firebase config (already valid for your project)
-import { firebaseConfig } from "./config.js";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAFe3jkeenflwT1boIfiYYJzseImeoB8gg",
-//   authDomain: "report-e8db1.firebaseapp.com",
-//   projectId: "report-e8db1",
-//   storageBucket: "report-e8db1.firebasestorage.app",
-//   messagingSenderId: "484990595847",
-//   appId: "1:484990595847:web:f0c319c020f1636ea14ba2",
-//   measurementId: "G-SHW5K7DDK4"
-// };
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
 
 // ✅ Initialize Firebase & Firestore
 const app = initializeApp(firebaseConfig);
