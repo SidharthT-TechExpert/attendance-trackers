e36ab9051bcf953d42918804693c638b87adda7d
Midhlaj vk <midhlajrocks@gmail.com>
2025-09-26 14:23:25 +0530

    First Push New Version of Attendence tracker. The new version name will be 'Attendify'.

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+require('dotenv').config()
+
+app.set('view engine','ejs')
+app.set('views',[
+    path.join(__dirname, 'views')
+])
+
+app.listen(process.env.PORT, () => {
+    console.log('Server Running')
+})
